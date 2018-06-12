13cdd8a20a1e2cec05c92b04214a9b613c05586d
awanegenebayo <awanegenebayo@gmail.com>
2018-06-12 10:01:40 +0200

    Fixed startup script link for visual compass, added compass offsets

--- tools/start_visual_compass.php
+++ tools/start_visual_compass.php
@@ -7,7 +7,7 @@ if (count($argv) >= 2) {
   $host = $argv[1];
 }
 
-include "robot_tools.php"
+include "robot_tools.php";
 
 msg('INIT', 'Press enter to run init');
 readline();
@@ -15,11 +15,14 @@ cmd('init');
 
 msg('Overwriting head parameters for visual compass');
 cmd('/moves/head/forceCompass=true');
-cmd('/moves/head/compassMaxPan=210');
+cmd('/moves/head/compassMaxPan=240');
 cmd('/moves/head/vcMaxSpeed=20');
+cmd('/moves/head/disabled=false');
+cmd('head');
+
 
 msg('Reducing framerate to 5 FPS');
 cmd('/Vision/source/FrameRate/absValue=5');
 
-msg('Starting manual Log for 10 seconds');
-cmd('logLocal 15');
+msg('Starting manual Log for 15 seconds');
+cmd('logLocal 20');
