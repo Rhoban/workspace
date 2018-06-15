e103b788feffd3710def9c3d3d2c841069c8442a
Gregwar <g.passault@gmail.com>
2018-06-15 22:16:22 +0200

    Adding hash script

--- .workspace/HashCommand.php
+++ .workspace/HashCommand.php
@@ -15,6 +15,7 @@ class HashCommand extends Command
     public function run(array $arguments)
     {
         foreach ($arguments as $repository) {
+            echo $repository.': ';
             OS::run('cd src/'.$repository.'; git rev-parse HEAD');
         }
     }
