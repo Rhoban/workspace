f71195290ba0e0b59b1a14e4f6b22aab904f3711
Patxi Laborde-Zubieta <patxi.laborde.zubieta@protonmail.com>
2019-06-11 14:19:46 +0200

    Adding debug in chrony_monitoring.

--- tools/chrony_monitoring.py
+++ tools/chrony_monitoring.py
@@ -5,7 +5,9 @@ from time import sleep
 import datetime
 
 if len(sys.argv) < 2:
-    print('Usage: time_client.pt [host1] [host2]')
+    print('Usage: time_client.pt [host1] [host2] ...')
+    print('host\# should be the name of the robot.')
+    print('/etc/hosts should contain the conrrespondance between the robot\'s name and its ip.')
     exit()
 
 hosts = []
@@ -20,7 +22,7 @@ while True:
     msg += str(local_time)
     msg += " "
     for host in hosts:
-        offset = float(os.popen("ssh rhoban@%s chronyc -n -c sources" %host).readline().split(",")[-3])
+        offset = float(os.popen("ssh rhoban@%s chronyc -n -c sources" %host).readline().split(",")[-2])
         msg += str(offset * 1000)
         msg += " "
     print(msg[:-1])
