7001a7c8452fbf03a48bf7aa9627bb13e6912a3e
medrimonia <medrimonia@gmail.com>
2019-06-20 19:58:05 +0200

    Updating game_log_downloader, now can download both, game logs and manual logs

--- tools/game_log_downloader.py
+++ tools/game_log_downloader.py
@@ -14,34 +14,39 @@ def downloadGameLogs(host, game_log_path, dst):
     robot_name = getRobotName(host)
     team_id = "team9"
     robot_path = dst + "/" + team_id + "/" + robot_name
-    perception_path = robot_path + "/perception"
     output_log_path = robot_path + "/output_log"
     print("Checking existence of folder '" + game_log_path + "' on robot")
     checkFolder(host, game_log_path)
     output_logs = game_log_path + "/*.log"
-    video_logs = game_log_path + "/*/"
     systemOrRaise(["mkdir", "-p", output_log_path])
     print("Copying output logs from '" + output_logs + "'")
     scpCmd(host, output_logs, output_log_path)
+    perception_path = robot_path + "/perception"
     systemOrRaise(["mkdir", "-p", perception_path])
+    video_logs = game_log_path + "/*/"
     print("Copying video logs from '" + video_logs + "'")
     scpCmd(host, video_logs, perception_path)
-    #TODO: move folder to tmp/robot_name/perception and tmp/robot_name/output_logs before creating arxiv
-    #This part was suited for old logs format (png) but not for new (avi)
-    #print("Compressing folder on robot (might last several minutes)")
-    #sshCmd(host, "tar -czf " + compress_target + " " + game_log_path)
-    #print("Downloading " + compress_target + " from " + robot_name)
-    #tmp_local_arxiv = dst + "/" + compress_target
-    #scpCmd(host, compress_target, tmp_local_arxiv)
-    #print("Extracting data")
-    #systemOrRaise(["tar","-xzf ",tmp_local_arxiv])
-    #print("Moving data")
-    #TODO: move data (require to see/check structure)
-    # - Currently all the data are placed in 
-    #TODO: clean data
 
+def downloadManualLogs(host, log_path, dst):
+    # Getting name + preparing folder names
+    robot_name = getRobotName(host)
+    team_id = "team9"
+    robot_path = dst + "/" + team_id + "/" + robot_name
+    print("Checking existence of folder '" + log_path + "' on robot")
+    checkFolder(host, log_path)
+    perception_path = robot_path + "/perception"
+    systemOrRaise(["mkdir", "-p", perception_path])
+    video_logs = log_path + "/*/"
+    print("Copying video logs from '" + video_logs + "'")
+    scpCmd(host, video_logs, perception_path)
+
+    
 def listGameLogs(host):
     return sshCmd(host, "find . -name \"game_logs\" | xargs du -hs | sort -h")
+
+def listManualLogs(host):
+    return sshCmd(host, "find . -name \"manual_logs\" | xargs du -hs | sort -h")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -52,14 +57,22 @@ if __name__ == "__main__":
                      help="Check if a log exist")
     cmd.add_argument("-d", "--download", type=str, nargs=2, metavar="LOG_PATH DST_PATH",
                      help="Download a log from LOG_PATH to the folder DST_PATH")
+    parser.add_argument("-m", "--manual", action="store_true",
+                        help="Focus on manual logs")
     parser.add_argument("robot", type=str,help="robots hostnames")
     args = parser.parse_args()
     robot = args.robot
     if args.list:
-        print(listGameLogs(robot))
+        if args.manual:
+            print(listManualLogs(robot))
+        else:
+            print(listGameLogs(robot))
     elif args.check:
         checkFolder(robot,args.check)
     elif args.download:
-        downloadGameLogs(robot, args.download[0], args.download[1])
+        if args.manual:
+            downloadGameLogs(robot, args.download[0], args.download[1])
+        else:
+            downloadManualLogs(robot, args.download[0], args.download[1])
     else:
         print ("no command specified")
