9da771425ea6dd87764bf423672a378efa606e92
medrimonia <medrimonia@gmail.com>
2019-06-21 15:25:58 +0200

    Updating start_vision_log_machine and chrony_monitoring

--- tools/chrony_monitoring.py
+++ tools/chrony_monitoring.py
@@ -3,27 +3,35 @@ import sys
 import os
 from time import sleep
 import datetime
+from robot_tools import *
+import argparse
 
-if len(sys.argv) < 2:
-    print('Usage: time_client.pt [host1] [host2] ...')
-    print('host\# should be the name of the robot.')
-    print('/etc/hosts should contain the conrrespondance between the robot\'s name and its ip.')
-    exit()
+def getChronyOffset(host):
+    """
+    Returns the time offset [s] with the given host
+    """
+    return float(sshCmd(host, "chronyc -n -c sources").split(",")[-2])
 
-hosts = []
-for host in sys.argv[1:]:
-    hosts.append(host)
+if __name__ == "__main__":
+    # Initial comments
+    #if len(sys.argv) < 2:
+    #    print('Usage: time_client.pt [host1] [host2] ...')
+    #    print('host\# should be the name of the robot.')
+    #    print('/etc/hosts should contain the conrrespondance between the robot\'s name and its ip.')
+    #    exit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hosts", type=str,help="hostnames", nargs="+")
+    args = parser.parse_args()
 
-initial_local_time = float(datetime.datetime.now().timestamp())
-while True:
-    sleep(0.5)
-    local_time = float(datetime.datetime.now().timestamp()) - initial_local_time
-    msg = ""
-    msg += str(local_time)
-    msg += " "
-    for host in hosts:
-        offset = float(os.popen("ssh rhoban@%s chronyc -n -c sources" %host).readline().split(",")[-2])
-        msg += str(offset * 1000)
+    initial_local_time = float(datetime.datetime.now().timestamp())
+    while True:
+        sleep(0.5)
+        local_time = float(datetime.datetime.now().timestamp()) - initial_local_time
+        msg = ""
+        msg += str(local_time)
         msg += " "
-    print(msg[:-1])
-    sys.stdout.flush()
+        for host in args.hosts:
+            msg += str(getChronyOffset(host) * 1000)
+            msg += " "
+        print(msg, end ='\n')
+        sys.stdout.flush()
--- tools/start_vision_log_machine.py
+++ tools/start_vision_log_machine.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+from chrony_monitoring import *
 from robot_tools import *
 import argparse
+
+# key: nb_robots
+# value: array with boundaries for each robot [minX maxX, minY maxY]
+boundariesNames = ["minX", "maxX", "minY", "maxY"]
+defaultBoundaries = { 4: [[-4, -0.5, -0.5,  2.5],
+                          [-4, -0.5, -2.5, -1.5],
+                          [0.5, 4.0, -0.5,  2.5],
+                          [0.5, 4.0, -2.5, -1.5]] }
 
 def setVariables(host, logDuration):
     msg('AUTO', "Setting all variables on " + host)
@@ -23,32 +32,66 @@ def setVariables(host, logDuration):
     msg('AUTO', "Reducing handledDelay")
     rhioCmd('decision/handledDelay=0.01', host)
     msg('AUTO', "Enabling odometryMode")
-    rhioCmd('/localisation/field/odometryMode=0', host)
+    rhioCmd('/localisation/field/odometryMode=1', host)
 
-def updateBoundaries(host):
+def printBoundaries(host):
+    for name in boundariesNames:
+        print(name + " -> " + rhioCmd("/moves/vision_log_machine/{:}".format(name)))
+
+def updateBoundaries(host, boundaries):
+    if (len(boundaries) != len(boundariesNames)):
+        raise RuntimeError("Boundaries len is not valid")
+    print("Setting boundaries for " + host)
+    for i in range(len(boundaries)):
+        name = boundariesNames[i]
+        value = boundaries[i]
+        print("-> {:} : {:}".format(name, value))
+        rhioCmd("/moves/vision_log_machine/{:}={:}".format(name,value), host)
+
+def customBoundaries(host):
     msg("UPDATE_BOUNDARIES", "Defining patrolling area for {:}".format(host))
-    boundaries = {"minX" : -4.5, "maxX" : 4.5, "minY" : -3, "maxY" : 3}
-    for k,v in boundaries.items():
-        newVal = askDouble("Enter value for {:} (default: {:})".format(k,v))
-        rhioCmd("/moves/vision_log_machine/{:}={:}".format(k,newVal), host)
+    boundaries = []
+    for i in range(len(boundariesNames)):
+        boundaries += [askDouble("Enter value for {:} )".format(boundariesNames[i]))]
+    updateBoundaries(host, boundaries)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--init", action="store_true",
                         help="initialize robots before launching log vision")
+    parser.add_argument("-c", "--customBoundaries", action="store_true",
+                        help="Ask the user to specify the custom boundaries")
+    parser.add_argument("-d", "--defaultBoundaries", action="store_true",
+                        help="Use default boundaries")
     parser.add_argument("hosts", type=str,help="hostname", nargs="+")
     args = parser.parse_args()
 
-    logDuration = askDouble("What is the required duration for log? [s]")
-    
+    msg("SYNC_CHECK", "Checking time offsets with the robots")
+    offsets = {}#[ms]
+    max_offset = 0;#[ms]
+    for host in args.hosts:
+        offsets[host] = 1000*getChronyOffset(host)
+        max_offset = max(abs(offsets[host]), max_offset)
+    print("measured offsets in ms: " + str(offsets))
+    if max_offset > 5:
+        print("measured offsets are too high, check sync")
+        exit(-1)
     if args.init:
         for host in args.hosts:
             defaultInit(host)
-    for host in args.hosts:
+
+    logDuration = askDouble("What is the required duration for log? [s]")
+    for host_idx in range(len(args.hosts)):
+        host = args.hosts[host_idx]
         setVariables(host, logDuration)
-        updateBoundaries(host)
+        if args.customBoundaries:
+            customBoundaries(host)
+        elif args.defaultBoundaries:
+            updateBoundaries(host, default_boundaries[host_idx])
+        else:
+            printBoundaries(host)
         manualCustomReset(host)
-    # TODO: define global areas
+
     msg("FINAL", "Press enter to activate vision_log_machine on all robots and start logging")
     sys.stdin.readline()
     for host in args.hosts:
